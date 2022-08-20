3b9fb838ce07cf675b60f54a4d1a7f3915b25e53
Abdelrahman Ashraf El-Khateeb <36169588+AbdelrahmanElKhateeb@users.noreply.github.com>
2022-08-20 22:21:12 +0000

    Rewrote the code from scratch

--- js/app.js
+++ js/app.js
@@ -1,66 +1,157 @@
-//const startingTime = performance.now();
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
 
-//sections : gets all sections in the document
-//navmenu : gets the navigation bar empty list
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+
+*/
+
+
+/**
+ * starting time >> Variable used to test performance
+ * section >> a variable for an empty object (to remove the error from Eslinter for section being undefined)
+ * sections >> gets all elements in the document that has section as selector and putsthem in this array, used for getting each section in the document and iteration
+ * navmenu >> gets the empty navigation list through its id
+*/
+
+const startingTime = performance.now();
+
+let section = new Object();
 let sections = [...document.querySelectorAll("section")];
 let navmenu = document.getElementById("navbar__list");
 
 
-//Adds new navigation bar items(sections) 
-function newNavItems(){
 
 
-    //nav : empty string to hold the html list element
-    let nav = '';
 
-    for(section of sections){
 
-        //populating the nav string with the html line that has the href for the section id and the section name
 
-        nav = nav + `<li><a class="menu__link" href="#${section.id}">${section.dataset.nav}</a></li>`;
-            
-        }
 
-        //after populating nav, we use it to fill navmenu (the empty one we got earlier) using .innerHTML
 
-        navmenu.innerHTML = nav;
 
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+/** A function that takes an element and gets its rectangular bounding box, and returns true if the element's distance to the top of the viewport is in the specified range */
+
+function InViewSection(element){
+    let x = element.getBoundingClientRect();
+        return (
+        (300 > x.top && x.top >= -300)
+    );
 }
 
-//intializes the function
 
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// Function that goes through the sections, takes the section id and name, puts them as a list item (listItem), edits their html using innerHTML, adds an anchor tag, then is added to the navigation bar list (navmenu)
+// href="#${section.id}"> Scrolls to section on link click 
+function newNavItems(){
+    let nav = '';
+    for(section of sections){
+        let listItem = document.createElement('li');
+        nav = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        listItem.innerHTML = nav;
+        navmenu.appendChild(listItem);
+      
+    }
+     
+    
+}
+
+
+// Goes through each section and to check if it is in view or not and removes or adds the class "active" respectively
+function toggleActiveClass(){
+    for(section of sections){
+
+        if(!InViewSection(section)){
+            // Removes class 'active' to section when near top of viewport
+            section.classList.remove('your-active-class');
+        
+        }
+
+        else{
+
+           if(InViewSection(section) && !section.classList.contains('your-active-class')){
+            // Adds class 'active' to section when near top of viewport and the section does not have it
+            section.classList.add('your-active-class');
+
+        }
+ 
+        }
+
+
+
+       
+
+
+
+
+}
+}
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build the navigation menu 
 newNavItems();
 
 
+//Event listener to call the function that checks if the section is in viewport
+window.addEventListener('scroll' , toggleActiveClass);
 
-
-//function controlling the activation and deactivation of sections using scrolling
-
-let ActivateSection = function(){
-    for(section of sections){
-
-        //for every section we get the boundingClientRect which gets the smallest rectangle which contains the contents of the element(section)
-        const BoundingOffset = Math.floor(section.getBoundingClientRect().top);
-
-
-        //Remove Activation : removing activation by removing "your-active-class" from class list
-        section.classList.remove('your-active-class');
-        section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,0.1) 0%, rgba(255,255,255,0.2) 100%)";
-
-
-        //Activate Section : if the smallest rectangle that we got is between these values, then we activate the section by adding "your-active-class" to the section's classlist and change the background color of the section
-        if(BoundingOffset < 220 && BoundingOffset >= -220){
-        section.classList.add('your-active-class');
-        section.style.cssText = "background-color: #4d004d;";
-        section.style.transition = "all 0.3s";
-    };
-    }
-
-};
-
-//Activates the function when the user scrolls whether directly, or indirectly(using anchors)
-window.addEventListener('scroll' ,ActivateSection);
 
 //Testing Performance
-//const endingTime = performance.now();
-//console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
+const endingTime = performance.now();
+console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set sections as active
+
+
