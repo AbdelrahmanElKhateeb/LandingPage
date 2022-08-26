9a4e192dfc926deb0cd27d959c4befc3cb8aaadc
Abdelrahman Ashraf El-Khateeb <36169588+AbdelrahmanElKhateeb@users.noreply.github.com>
2022-08-26 23:27:55 +0200

    Added Scrolling function and a bit more changes

--- js/app.js
+++ js/app.js
@@ -11,147 +11,116 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
-
-*/
+ */
 
 
-/**
- * starting time >> Variable used to test performance
- * section >> a variable for an empty object (to remove the error from Eslinter for section being undefined)
- * sections >> gets all elements in the document that has section as selector and putsthem in this array, used for getting each section in the document and iteration
- * navmenu >> gets the empty navigation list through its id
-*/
-
+//Start Global Variables
 const startingTime = performance.now();
-
 let section = new Object();
 let sections = [...document.querySelectorAll("section")];
 let navmenu = document.getElementById("navbar__list");
+// End Global Variables
 
 
 
 
-
-
-
-
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-/** A function that takes an element and gets its rectangular bounding box, and returns true if the element's distance to the top of the viewport is in the specified range */
-
-function InViewSection(element){
+// Start Helper Functions
+function InViewSection(element) {
+    // Gets the boundingrect for each section 
     let x = element.getBoundingClientRect();
-        return (
+    // Returns true if the section is in viewport
+    return (
         (300 > x.top && x.top >= -300)
     );
 }
 
 
 
+// Start of scrolling function
+function scrollfunction(listItem, section) {
+    listItem.addEventListener("click", function(e) {
+        // Prevents default anchor behavior
+        e.preventDefault();
+        // Smoothly scrolls to the top of the section
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: "smooth"
+        });
+    })
+}
+// End of scrolling function
+// End Helper Functions
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
-// Function that goes through the sections, takes the section id and name, puts them as a list item (listItem), edits their html using innerHTML, adds an anchor tag, then is added to the navigation bar list (navmenu)
-// href="#${section.id}"> Scrolls to section on link click 
-function newNavItems(){
+
+
+// Begin Main Functions
+function newNavItems() {
     let nav = '';
-    for(section of sections){
+    for (section of sections) {
+        // Create the li elements which are inside the ul
         let listItem = document.createElement('li');
         nav = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        // Insert the html text to the li
         listItem.innerHTML = nav;
+        // Appends the ul to the navigation menu
         navmenu.appendChild(listItem);
-      
+        // Calls the scrolling function
+        scrollfunction(listItem, section);
+
     }
-     
-    
+
+
 }
 
 
 // Goes through each section and to check if it is in view or not and removes or adds the class "active" respectively
-function toggleActiveClass(){
-    for(section of sections){
+function toggleActiveClass() {
+    // Gets the navigation item buttons
+    let menu_elements = document.querySelectorAll(".menu__link");
+    let counter = 0;
+    for (section of sections) {
 
-        if(!InViewSection(section)){
-            // Removes class 'active' to section when near top of viewport
+        // Removes class 'active' to section when near top of viewport. Also removes active link class for the navigation item button 
+        if (!InViewSection(section)) {
             section.classList.remove('your-active-class');
-        
+            menu_elements[counter].classList.remove("active_link");
+
+
+        } else {
+
+            // Adds class 'active' to section when near top of viewport and the section does not have it. Also adds active link class for the navigation item button 
+            if (InViewSection(section) && !section.classList.contains('your-active-class')) {
+                section.classList.add('your-active-class');
+                menu_elements[counter].classList.add("active_link");
+
+            }
         }
-
-        else{
-
-           if(InViewSection(section) && !section.classList.contains('your-active-class')){
-            // Adds class 'active' to section when near top of viewport and the section does not have it
-            section.classList.add('your-active-class');
-
-        }
- 
-        }
-
-
-
-       
-
-
-
-
-}
+        counter++;
+    }
 }
 
+// End Main Functions
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
-// Build the navigation menu 
+
+
+// Builds the navigation menu 
 newNavItems();
 
+// Begin Events
+// Event listener to call the function that checks if the section is in viewport
+window.addEventListener('scroll', toggleActiveClass);
+// End of Events
 
-//Event listener to call the function that checks if the section is in viewport
-window.addEventListener('scroll' , toggleActiveClass);
 
 
-//Testing Performance
+
+// Testing Performance
 const endingTime = performance.now();
 console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Set sections as active
-
-
